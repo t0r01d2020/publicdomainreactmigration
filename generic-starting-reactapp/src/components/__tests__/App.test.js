6d330635398e7b16b6d66f6129ea3f80a87b82e7
Bradley Sisk <bsisk@dminc.com>
2020-03-30 15:42:18 -0400

    Refactor the scope of our unit tests....make compatible w. Redux.

--- generic-starting-reactapp/src/components/__tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../App';
-
-test('renders buttons', () => {
-  const { getByText } = render(<App />);
-  const buttonElement = getByText(/A Bootstrap Button/i);
-  expect(buttonElement).toBeInTheDocument();
-});
