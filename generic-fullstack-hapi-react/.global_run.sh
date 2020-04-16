f67d3fca058ba19ed8b0bd910904cefbac3681b5
Sisk <extern.bradley.sisk@vw.com>
2020-04-16 16:07:55 -0400

    launch middleman in a separate process, and terminal tab

--- generic-fullstack-hapi-react/.global_run.sh
+++ generic-fullstack-hapi-react/.global_run.sh
@@ -6,11 +6,12 @@ echo "  "
 echo "Launching Redis in separate process..."
 redis-server /usr/local/etc/redis.conf &
 
+echo "Redis server launched..."
+echo "   "
 
-
-echo "First, launching the middleman in a separate process..."
+echo "First, launching the middleman in a separate process...and a separate Shell Tab..."
 echo "###########################################################################################"
-ttab node hapi_middleman/index.js &
+ttab node ./hapi_middleman/index.js &
 echo "  "
 echo "  "
 echo "Now, launching frontend..."
